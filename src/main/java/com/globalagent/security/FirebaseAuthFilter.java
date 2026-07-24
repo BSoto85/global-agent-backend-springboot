b318f1d48114be8a183fe635d6fc6bd769277de5
brenda-soto <bsoto@pursuit.org>
2026-07-24 15:36:01 -0400

    fix: remove @Component from FirebaseAuthFilter to avoid duplicate bean

--- src/main/java/com/globalagent/security/FirebaseAuthFilter.java
+++ src/main/java/com/globalagent/security/FirebaseAuthFilter.java
@@ -8,11 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 
-@Component
 public class FirebaseAuthFilter implements Filter {
 
     private static final Logger log = LoggerFactory.getLogger(FirebaseAuthFilter.class);
